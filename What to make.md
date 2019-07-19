a88b356060864a7d93859f534f7e7154a3094665
BuildTools <jackisgood301@gmail.com>
2019-07-18 21:13:34 -0400

    First commit

---
+++ What to make.md
@@ -0,0 +1,20 @@
+# What is yomama website
+
+Yo mama will organize yo mama jokes into categories.
+
+Categories are arrays of objects.
+
+The main file would look like this:
+
+```json
+{
+  "fat": [
+    {
+      "joke": "Yo mama so fat, not even Ninja could carry her in Fortnite!",
+      "url": "https://www.youtube.com/embed/g8jWF6P-8jY?start=19"
+    }
+  ]
+}
+```
+
+Or it could be split into more than one file.
---
+++ main.js
@@ -0,0 +1,37 @@
+/*
+The code below *is* my code
+Please don't steal it.
+The jokes are from the Yo Mama channel
+*/
+(async function () {
+  let jokes = await (window.fetch('jokes.json').then(r => r.json())) // Get the jokes
+
+  let m = function (a) { // This function returns the maker function so I don't need to copy paste my changes to every one
+    return function () { // Return the function
+      let element = document.createElement('div') // Create the holder element
+
+      let joke = document.createElement('h2') // Create the joke element
+      let yomama = jokes[a][Math.floor(Math.random() * jokes[a].length)] //  yomama is the joke object
+
+      joke.innerText = yomama.joke // Change its text to a random fat joke
+      element.appendChild(joke) // Add the joke element to the holder
+
+      let embed = document.createElement('iframe')
+      embed.src = yomama.url // Set the URL to the yo mama joke's url
+      embed.width = '560' // Set the width to the correct amount
+      embed.height = '315' // Set the height to the correct amount
+      embed.allow = 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' // Give it the right permissions
+      embed.frameBorder = 0 // Remove the border
+      element.appendChild(embed) // Add the video to the holder
+
+      document.body.appendChild(element) // Add the holder to the body
+    }
+  }
+
+  Object.keys(jokes).forEach((name) => { // For every joke category
+    let button = document.createElement('button') // Make a button element
+    button.innerText = name.charAt(0).toUpperCase() + name.slice(1) // Make the name of the button the category name but the first letter is uppercase
+    button.onclick = m(name) // Add the onclick event
+    document.body.appendChild(button) // Add it to the body
+  })
+})()
