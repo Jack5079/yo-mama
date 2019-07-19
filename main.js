39fc4e016769fb1be7cd0fbefcf9ca1bc84c87e0
BuildTools <jackisgood301@gmail.com>
2019-07-18 21:27:13 -0400

    add error handling

--- main.js
+++ main.js
@@ -7,24 +7,29 @@ The jokes are from the Yo Mama channel
   let m = function (a) { // This function returns the maker function so I don't need to copy paste my changes to every one
     return function () { // Return the function
       let element = document.createElement('div') // Create the holder element
-      let yomama = jokes[a][Math.floor(Math.random() * jokes[a].length)] //  yomama is the joke object
+      if (jokes[a]) {
+        let yomama = jokes[a][Math.floor(Math.random() * jokes[a].length)] //  yomama is the joke object
 
-      if (yomama.joke) {
-        let joke = document.createElement('h2') // Create the joke element
-        joke.innerText = yomama.joke // Change its text to a random fat joke
-        element.appendChild(joke) // Add the joke element to the holder
+        if (yomama.joke) {
+          let joke = document.createElement('h2') // Create the joke element
+          joke.innerText = yomama.joke // Change its text to a random fat joke
+          element.appendChild(joke) // Add the joke element to the holder
+        }
+
+        if (yomama.url) {
+          let embed = document.createElement('iframe')
+          embed.src = yomama.url // Set the URL to the yo mama joke's url
+          embed.width = '560' // Set the width to the correct amount
+          embed.height = '315' // Set the height to the correct amount
+          embed.allow = 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' // Give it the right permissions
+          embed.frameBorder = 0 // Remove the border
+          element.appendChild(embed) // Add the video to the holder
+        }
+      } else {
+        let error = document.createElement('h2')
+        error.innerText = 'No yo mama jokes were found for that category. Sorry!'
+        element.appendChild(error)
       }
-
-      if (yomama.url) {
-        let embed = document.createElement('iframe')
-        embed.src = yomama.url // Set the URL to the yo mama joke's url
-        embed.width = '560' // Set the width to the correct amount
-        embed.height = '315' // Set the height to the correct amount
-        embed.allow = 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' // Give it the right permissions
-        embed.frameBorder = 0 // Remove the border
-        element.appendChild(embed) // Add the video to the holder
-      }
-
       document.body.appendChild(element) // Add the holder to the body
     }
   }
