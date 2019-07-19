7ac12dd588fb19222aa7ec34b17fab664ceb9d6a
BuildTools <jackisgood301@gmail.com>
2019-07-18 21:30:48 -0400

    better error handling

--- main.js
+++ main.js
@@ -7,15 +7,12 @@ The jokes are from the Yo Mama channel
   let m = function (a) { // This function returns the maker function so I don't need to copy paste my changes to every one
     return function () { // Return the function
       let element = document.createElement('div') // Create the holder element
+      try {
       let yomama = jokes[a][Math.floor(Math.random() * jokes[a].length)] //  yomama is the joke object
-
-      if (yomama.joke) {
         let joke = document.createElement('h2') // Create the joke element
         joke.innerText = yomama.joke // Change its text to a random fat joke
         element.appendChild(joke) // Add the joke element to the holder
-      }
 
-      if (yomama.url) {
         let embed = document.createElement('iframe')
         embed.src = yomama.url // Set the URL to the yo mama joke's url
         embed.width = '560' // Set the width to the correct amount
@@ -23,6 +20,8 @@ The jokes are from the Yo Mama channel
         embed.allow = 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' // Give it the right permissions
         embed.frameBorder = 0 // Remove the border
         element.appendChild(embed) // Add the video to the holder
+      } catch {
+        element.innerHTML = '<h2>There\'s no yo mama jokes for that category. Sorry!'
       }
       document.body.appendChild(element) // Add the holder to the body
     }
