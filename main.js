c43b87ff03cc060ddc3335366f8d138469e905b2
BuildTools <jackisgood301@gmail.com>
2019-07-23 19:25:19 -0400

    Better function names

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ The jokes are from the Yo Mama channel
   let jokefile = await window.fetch('jokes.json') // Get the joke file
   let jokes = await jokefile.json() // Convert the jokes to JSON
 
-  let m = function (a) { // This function returns the maker function so I don't need to copy paste my changes to every one
+  let addjoke = function (category) { // This function returns the maker function so I don't need to copy paste my changes to every one
     return function () { // Return the function
       let element = document.createElement('div') // Create the holder element
-      let yomama = jokes[a][Math.floor(Math.random() * jokes[a].length)] //  yomama is the joke object
+      let yomama = jokes[category][Math.floor(Math.random() * jokes[category].length)] //  yomama is the joke object
       element.innerHTML = `
       <h2>${yomama.joke}</h2>
       <iframe src="${yomama.url}" width="560" height="315" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" frameborder="0"></iframe>
@@ -19,7 +19,7 @@ The jokes are from the Yo Mama channel
   Object.keys(jokes).forEach(name => { // For every joke category
     let button = document.createElement('button') // Make a button element
     button.innerText = name.charAt(0).toUpperCase() + name.slice(1) // Make the name of the button the category name but the first letter is uppercase
-    button.onclick = m(name) // Add the onclick event
+    button.onclick = addjoke(name) // Add the onclick event
     document.body.appendChild(button) // Add it to the body
   })
 })()
