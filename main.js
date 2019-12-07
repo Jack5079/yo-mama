45b000767e1caa9939caad6af2eeb9aa22c2a083
Jack <29169102+Jack5079@users.noreply.github.com>
2019-12-06 20:30:46 -0500

    Fix it

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 // Code for jack5079.github.io/yo-mama/
-
 let jokes = { // The joke list
   fat: [ // Fat jokes
     {
@@ -19,29 +18,29 @@ let jokes = { // The joke list
   ]
 }
 
-Object.keys(jokes).forEach(name => { // For every joke category
-  let button = document.createElement('button') // Make a button element
-  button.innerText = name.charAt(0).toUpperCase() + name.slice(1) // Make the name of the button the category name but the first letter is uppercase
-  button.onclick = function () { // Add the onclick event
-    let jokeHolder = document.createElement('div') // Create the holder element
-    let yomama = jokes[name][Math.floor(Math.random() * jokes[name].length)] //  yomama is the joke object
+Object.keys( jokes ).forEach( name => { // For every joke category
+  let button = document.createElement( 'button' ) // Make a button element
+  button.innerText = name.charAt( 0 ).toUpperCase() + name.slice( 1 ) // Make the name of the button the category name but the first letter is uppercase
+  button.onclick = (function () { // Add the onclick event
 
-    let title = document.createElement('h2')
-    title.innerText = `Yo mama so ${name}, ${yomama.joke}`
-    jokeHolder.appendChild(title)
-    if (yomama.hasOwnProperty('url')) { // If the url is there
-      let video = document.createElement('iframe')
+    let jokeHolder = document.createElement( 'div' ) // Create the holder element
+    let yomama = jokes[ name ][ Math.floor( Math.random() * jokes[ name ].length ) ] //  yomama is the joke object
+
+    let title = document.createElement( 'h2' )
+    title.innerText = `Yo mama so ${ 'name' }, ${ yomama.joke }`
+    jokeHolder.appendChild( title )
+      let video = document.createElement( 'iframe' )
 
       video.src = yomama.url
 
       video.allowFullscreen = true
       video.frameBorder = '0'
-      video.allow = 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+      video.allow = 'encrypted-media  ; picture-in-picture'
       video.width = '560'
       video.height = '315'
 
-      jokeHolder.appendChild(video)
-    }
-  }
-  document.body.appendChild(button) // Add it to the body
-})
+      jokeHolder.appendChild( video )
+      document.body.appendChild(jokeHolder)
+  } )
+  document.body.appendChild( button ) // Add it to the body
+} )
